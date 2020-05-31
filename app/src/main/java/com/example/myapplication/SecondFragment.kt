02bd29891398cfcc68f695ded30cf3e4bae75211
mousumkumar <mousumkumar@gmail.com>
2020-06-01 01:13:09 +0530

    UI improvements 0.3.

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication
 
+import android.app.ActionBar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewParent
 import android.widget.Button
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.assigned_dialog.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -30,12 +33,17 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        view.findViewById<Button>(R.id.add).setOnClickListener {
+            val title = dialog_title.text.toString()
+            val description = dialog_description.text.toString()
+
+            val uid = FirebaseAuth.getInstance().uid
+
+            FirebaseDatabase.getInstance().reference.child(uid!!).child("assigned")
+                .push()
+                .setValue(Tasks(title, description))
+
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
-
-        val task = Tasks("Task", "Description")
-        val uid = FirebaseAuth.getInstance().uid
-        FirebaseDatabase.getInstance().reference.child(uid!!).child("assigned").push().setValue(task)
     }
 }
