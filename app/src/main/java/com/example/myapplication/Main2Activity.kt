b47370dd04544075a2cfaf1e976688e79497becc
mousumkumar <mousumkumar@gmail.com>
2020-06-01 02:41:09 +0530

    Signout implemented.

--- app/src/main/java/com/example/myapplication/Main2Activity.kt
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -4,10 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListAdapter
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.AuthUI
+import kotlinx.android.synthetic.main.activity_main2.view.*
 
 class Main2Activity : AppCompatActivity() {
 
@@ -43,5 +47,18 @@ class Main2Activity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_logout) {
+            AuthUI.getInstance().signOut(this)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
